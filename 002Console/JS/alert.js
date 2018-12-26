2e4436a093cc8260a59478b2cd4a30a4eebc3726
momomelody <2700634353@qq.com>
2018-12-26 11:17:41 +0800

    modify

---
+++ 002Console/JS/alert.js
@@ -0,0 +1,15 @@
+/**
+ * Created by COW on 2018/12/26.
+ */
+function wotaolu(){
+    alert("youtoo>>>i'm genius");
+    alert("metoo>>>i'm genius");
+}
+/*alert("子曰：\n"i'm genius"");*/
+alert("i'm genius one");
+
+wotaolu();
+wotaolu();
+wotaolu();
+
+alert("i'm genius two");
